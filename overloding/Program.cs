c5034677997c75580a89e3fb0b0978906764dbaa
Vishal Prajapati <vishalp951295@gmail.com>
2024-07-12 20:32:07 +0530

    Add project files.

---
+++ overloding/Program.cs
@@ -0,0 +1,19 @@
+﻿using overloding;
+
+overlode overlode = new();
+
+overlode.addcity("vadodra");
+
+List <string> city_name = new List<string>();
+city_name.Add("anand");
+city_name.Add("nadiad");
+city_name.Add("dahod");
+
+overlode.addcity(city_name);
+
+foreach (string p in overlode.cities)
+{
+    Console.WriteLine(p);
+}
+
+overlode.find("anand","vadodra");
---
+++ overloding/overlode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloding
+{
+    public class overlode
+    {
+      
+      public List<string> cities = new List<string>();
+
+        public void addcity(string name)
+        {
+            cities.Add(name);
+   
+        }
+        public void addcity(List<string> name)
+        {
+            cities.AddRange(name);
+        }
+        public void find(string city)
+        {
+            Console.WriteLine("enter one city name: ");
+            
+            foreach (string n in cities)
+            {
+                if (city == n)
+                {
+                    Console.WriteLine(city);
+                }
+            }
+        }
+        public void find(string city, string city1)
+        {
+            foreach (string n in cities)
+            {
+                if(city == n || city1 == n)
+                {
+                    Console.WriteLine(city);
+                }
+            }
+        }
+        public void find(List<string> city)
+        {
+            foreach(string name in cities)
+            {
+                if(city.Contains(name))
+                {
+                    Console.WriteLine($"{name}");
+                }
+            }
+        }
+    }
+}
